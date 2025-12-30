ce2fcf2a39cacb9d40d933d96d1ffc7e75063c2d
Lucas Rodrigues <175415849+Trindadelucas0@users.noreply.github.com>
2025-12-30 15:32:20 -0300

    remov

---
+++ middleware/csrfHelper.js
@@ -0,0 +1,20 @@
+/**
+ * Helper para CSRF Token
+ * Disponibiliza token CSRF nas views
+ */
+
+module.exports = (req, res, next) => {
+  // Tenta gerar token CSRF se sessão existir
+  if (req.session && req.session.user) {
+    try {
+      res.locals.csrfToken = req.csrfToken ? req.csrfToken() : null;
+    } catch (error) {
+      // Se não conseguir gerar token, define como null
+      res.locals.csrfToken = null;
+    }
+  } else {
+    res.locals.csrfToken = null;
+  }
+  next();
+};
+
